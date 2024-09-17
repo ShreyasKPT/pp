314e3eafec93917440c203e466897f74ed970228
ShreyasKPT <162275316+ShreyasKPT@users.noreply.github.com>
2024-09-17 10:09:30 +0530

    Add files via upload

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here is my first project I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Hometown"
+              description="This is my first website on my hometown named kadaba .I made this site using html and css only.so I inserted all the places,schools and best spots and agriculture on it..."
+              ghLink="https://github.com/ShreyasKPT/website.html"
+              
+              
+            />
+          </Col>
+
+          
+
+          
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
